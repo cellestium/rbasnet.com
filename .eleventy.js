e39e7f13d34c5da5e890b00b401fffe5601e96a3
notahpspell <kajirojanbasnet@gmail.com>
2025-10-12 16:41:21 -0400

    Create .eleventy.js

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function(eleventyConfig) {
+  // Copy your static assets straight through
+  eleventyConfig.addPassthroughCopy({"src/css": "css"});
+  eleventyConfig.addPassthroughCopy({"src/js": "js"});
+  eleventyConfig.addPassthroughCopy({"src/fonts": "fonts"});
+  eleventyConfig.addPassthroughCopy({"src/images": "images"});
+  eleventyConfig.addPassthroughCopy({"src/videos": "videos"});
+  eleventyConfig.addPassthroughCopy({"src/documents": "documents"});
+  eleventyConfig.addPassthroughCopy({"src/admin": "admin"});
+
+  return {
+    dir: { input: "src", output: "_site" }
+  };
+};
