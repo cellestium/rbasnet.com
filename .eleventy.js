35f2d4481dd976a00fd211646c9934524ea7315d
notahpspell <kajirojanbasnet@gmail.com>
2025-10-17 00:37:44 -0400

    Add projects collection to Eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy({"src/videos": "videos"});
   eleventyConfig.addPassthroughCopy({"src/documents": "documents"});
   eleventyConfig.addPassthroughCopy({"src/admin": "admin"});
+  eleventyConfig.addCollection("projects", c =>
+  c.getFilteredByGlob("src/content/projects/**/*.md")
+);
 
   return {
     dir: { input: "src", output: "_site" }
